2b0c049e59a3e5c935eeb3ff3cb81bc5894f31f9
Viktoriia <svinarenkoviki@gmail.com>
2020-01-30 21:11:32 +0200

    first commit

--- cards.js
+++ cards.js
@@ -1,44 +1,43 @@
 export default
 [
 	{
-		 "name": "Ксюша", 
-		 "city": "Полтава", 
-		 "reason": "Хочу веселья", 
-		 "company": "Больше 5 человек", 
-		 "place": "Villa Krokodila", 
-		 "drink": "Коктейли"
+		"name": "Ксюша", 
+		"city": "Полтава", 
+		"reason": "Хочу веселья", 
+		"company": "Больше 5 человек", 
+		"place": "Villa Krokodila", 
+		"drink": "Коктейли"
 	}, 
 	{
-		 "name": "Андрей", 
-		 "city": "Полтава", 
-		 "reason": "Выборы президента", 
-		 "company": "3 человека", 
-		 "place": "Villa Krokodila", 
-		 "drink": "Пиво"
+		"name": "Андрей", 
+		"city": "Полтава", 
+		"reason": "Выборы президента", 
+		"company": "3 человека", 
+		"place": "Villa Krokodila", 
+		"drink": "Пиво"
 	}, 
 	{
-		 "name": "Зинаида", 
-		 "city": "Полтава", 
-		 "reason": "День рождения", 
-		 "company": "1 человек", 
-		 "place": "Versal", 
-		 "drink": "Водка"
+		"name": "Зинаида", 
+		"city": "Полтава", 
+		"reason": "День рождения", 
+		"company": "1 человек", 
+		"place": "Versal", 
+		"drink": "Водка"
 	}, 
 	{
-		 "name": "Виолетта", 
-		 "city": "Полтава", 
-		 "reason": "Просто так", 
-		 "company": "3 человека", 
-		 "place": "Чичиков", 
-		 "drink": "Вино"
+		"name": "Виолетта", 
+		"city": "Полтава", 
+		"reason": "Просто так", 
+		"company": "3 человека", 
+		"place": "Чичиков", 
+		"drink": "Вино"
 	}, 
 	{
-		 "name": "Виктор", 
-		 "city": "Полтава", 
-		 "reason": "Найти друзей", 
-		 "company": "2 человека", 
-		 "place": "Muza Bar", 
-		 "drink": "Коктейли"
+		"name": "Виктор", 
+		"city": "Полтава", 
+		"reason": "Найти друзей", 
+		"company": "2 человека", 
+		"place": "Muza Bar", 
+		"drink": "Коктейли"
 	}
-	
 ]
